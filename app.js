b03daec89db0d519ba1bcb17f6ea8bf4f94e9553
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-05-30 12:54:48 +0100

    imported modules and starter file

---
+++ app.js
@@ -0,0 +1,14 @@
+// Import required modules
+const express = require('express');
+
+//Create instance of express app
+const app = express();
+const port = 4000;
+
+// Register view engine
+app.set('view engine','ejs');
+
+app.listen(port, () => console.log(`Example app listening on port port!`))
+
+// Test for index route
+app.get('/', (req, res) => res.send('Hello World!'));
