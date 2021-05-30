f53726705fe6919c3d0d3af3a4977d9242961f6c
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-05-30 13:45:04 +0100

    set up middleware and static files

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // Import required modules
 const express = require('express');
+const morgan = require('morgan');
 
 //Create instance of express app
 const app = express();
+// Set port to 4000
 const port = 4000;
 
 // Register view engine
 app.set('view engine','ejs');
 
+// Middleware and Static files
+app.use(morgan('dev'));
+app.use(expres.static('public'));
+
+// Listen for port 4000
 app.listen(port, () => console.log(`Example app listening on port port!`))
 
 // Test for index route
