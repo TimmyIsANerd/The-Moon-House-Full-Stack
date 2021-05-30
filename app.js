d5183ff291dd91eac1c9841f3ee1eb2ed05e6ef9
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-05-30 15:21:02 +0100

    set up new routes

--- app.js
+++ app.js
@@ -16,9 +16,24 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 
 // Listen for port 4000
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
 
-// Test for index route
+// Home Route
 app.get('/', (req, res) =>{
     res.render('index');
 });
+app.get('/home',(req,res)=>{
+    res.redirect('/')
+});
+// Invest Page Route
+app.get('/invest', (req,res)=>{
+    res.render('invest');
+})
+// Contact Us Route
+app.get('/contact', (req,res)=>{
+    res.render('contact');
+});
+// Privacy Policy Route
+app.get('/policy',(req,res)=>{
+    res.render('privacy');
+})
