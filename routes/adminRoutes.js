3c2f73096ce4416b26f2ec94806509833521f619
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-04 12:40:46 +0100

    set up routes

---
+++ routes/adminRoutes.js
@@ -0,0 +1,9 @@
+// Import Express
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/adminController');
+
+// Import Express Router
+
+// Setup Routes related to admin
+
