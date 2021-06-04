e6be05b3324b5bfa60917f3790c77ce373172b5e
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-04 12:38:59 +0100

    set up controllers

---
+++ controllers/loginController.js
@@ -0,0 +1,10 @@
+
+// Define Login Controller
+const login_get = (req,res) =>{
+    res.render('./userLogin/userLogin');
+}
+
+ 
+module.exports = {
+    login_get
+}
---
+++ controllers/signUpcontroller.js
@@ -0,0 +1,48 @@
+// Import Sign Up Model
+const userSignUp = require('../model/userSignUp');
+// Import Bcrypt
+const bcrypt = require('bcryptjs');
+// Set Up Sign Up Controller
+// GET Sign Up Page
+const sign_up_get = (req,res) =>{
+    res.render('./userSignUp/userSignUp');
+}
+// POST Sign Up Page
+const sign_up_post = async (req,res)=>{
+    console.log(req.body);    
+    const { fullname,email, password: plainTextPassword } = req.body;
+    // Hashing Password using bcrypt library
+    // Password Validation
+    if(!email || typeof email !== 'string'){
+        return res.json({status : error, error : 'Invalid Email'});
+    }
+
+    if(!plainTextPassword || typeof plainTextPassword !== 'string'){
+        return res.json({status : error, error : 'Invalid Password'});
+    }
+    if(plainTextPassword.length < 5){
+        return res.json({status : error, error : '{Password is less than 6 characters}'});
+    }
+    const password = await bcrypt.hash(plainTextPassword, 10);
+    
+    try{
+        const response = await userSignUp.create({
+            fullname,
+            email,
+            password
+        })
+        console.log('User Created Successfully', response);
+    } catch (error){
+        if(error.code === 11000){
+            // duplicate key
+            return res.json({ status : 'error', error : 'Email already in use' });
+        }
+        throw error
+    }
+    res.json({status : 'ok' });
+}
+
+module.exports = {
+    sign_up_get,
+    sign_up_post
+}
