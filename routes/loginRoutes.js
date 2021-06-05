c00efacf0024f5d7276c1cd0983f1ebfc48e844b
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-05 08:08:23 +0100

    created new views

--- routes/loginRoutes.js
+++
@@ -1,9 +0,0 @@
-// Import Express Router
-const express = require('express');
-const router = express.Router();
-// Import Controller
-const loginController = require('../controllers/loginController');
-
-router.get('/user/login',loginController.login_get);
-
-module.exports = router;
