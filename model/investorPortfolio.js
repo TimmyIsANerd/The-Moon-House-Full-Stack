3028adef47d267d1b3fa59e143e1f723a19bc7ec
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-06 00:35:30 +0100

    set up investor portfolio model

---
+++ model/investorPortfolio.js
@@ -0,0 +1,32 @@
+// Require Mongoose
+const mongoose = require('mongoose');
+
+// Create Investor Portfolio (Investor Plan and Length)
+
+const investorPortfolio = new mongoose.Schema(
+    {
+        investmentType : {
+            type : String,
+            required:true,
+        },
+        investmentDuration : {
+            type: Number,
+            required:true
+        },
+        investmentPaymentMethod:{
+            type:String,
+            required:true
+        },
+        InvestmentWithdrawalMethod:{
+            type:String,
+            required:true
+        }
+    },
+    { collection : 'user_signup_data'},
+    { timestamps : true}
+)
+
+// Export Schema
+
+const investorPortfolio = mongoose.model('investorPortfolio',investorPortfolio);
+module.exports = investorPortfolio;
