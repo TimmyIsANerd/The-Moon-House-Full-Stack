77f32cacedde8da6445fdb016343575c5a80d6a5
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-05 08:03:50 +0100

    made changes to routes

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const User = require('./model/user');
-const bcrypt = require('bcryptjs');
+
+//Create instance of express app
+const app = express();
+
+// Import Express Routes
+const basicRoutes = require('./routes/basicRoutes');
+// Authentication Routes
+const authRoutes = require('./routes/authRoutes');
 
 // Connect MongoDB
+// After Mongo Connection is successful, start listening on open port.
 const dbURI = 'mongodb://127.0.0.1:27017/the_moon_house';
 mongoose.connect(dbURI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-})
+}).then((result) =>app.listen(port, () => console.log(`The Moon House App listening on port ${port}`))).catch((err) => console.log(err));
 
-//Create instance of express app
-const app = express();
 // Set port to 4000
 const port = 4000;
 
@@ -28,76 +33,10 @@ app.use(express.static('public'));
 // Middleware to decode incoming body
 app.use(bodyParser.json());
 
-// Listen for port 4000
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+// Use Express Routes
+app.use(basicRoutes);
+app.use(authRoutes);
 
-
-
-// Set Up Routes
-// Home Route
-app.get('/', (req, res) =>{
-    res.render('index');
-});
-app.get('/home',(req,res)=>{
-    res.redirect('/');
-});
-// Invest Page Route
-app.get('/invest', (req,res)=>{
-    res.render('invest');
-})
-// Invest Sign Up Route
-app.get('/invest/signup',(req,res)=>{
-    res.render('invest_signup');
-})
-app.get('/invest/login',(req,res)=>{
-    res.render('invest_login');
-})
-// Academy Route
-app.get('/academy',(req,res)=>{
-    res.render('academy');
-});
-app.get('/academy/signup',(req,res)=>{
-    res.render('academy_signup');
-});
-// JSON Request handler
-app.post('/api/register', async (req,res)=>{
-    console.log(req.body);
-    // Hashing Password using bcrypt library
-    const { email, password: plainTextPassword } = req.body;
-    const password = await bcrypt.hash(plainTextPassword, 10);
-    
-    try{
-        const response = await User.create({
-            email,
-            password
-        })
-        console.log('User Created Successfully', response)
-    } catch (error){
-        console.log(error)
-        return res.json({ status : 'error' });
-    }
-
-    res.json({status : 'ok' })
-})
-app.get('/academy/login',(req,res)=>{
-    res.render('academy_login');
-})
-// Contact Us Route
-app.get('/contact', (req,res)=>{
-    res.render('contact');
-});
-// Privacy Policy Route
-app.get('/policy',(req,res)=>{
-    res.render('privacy');
-});
-// Terms & Conditions Route
-app.get('/terms',(req,res)=>{
-    res.render('terms');
-});
-// Admin Route
-app.get('/admin',(req,res)=>{
-    res.render('admin',{admin:'admin'});
-})
 // 404 Route
 app.use((req,res)=>{
     res.status(404).render('404');
