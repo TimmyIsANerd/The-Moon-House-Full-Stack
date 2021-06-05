f61cc44ac5646eab17acb77f62145587b699b7ba
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-06 00:33:26 +0100

    investor know your customer model

---
+++ model/investor_kyc.js
@@ -0,0 +1,23 @@
+// Require Mongoose 
+const mongoose = require('mongoose');
+// Import Validator
+
+// Create Investor Know Your Customer Schema
+const investorKyc = new mongoose.Schema(
+    {   
+        // Specify Needed KYC Information
+        homeAddress : {type: String,required:[true,'Please Enter valid Address']},
+        phoneAddress : {type:Number,required:[true,'Please Enter valid Phone Number']},
+        // Next of Kin
+        kinFullName : {type:String,required:[true,'Please Enter a Valid Name']},
+        kinHomeAddress : {type: String,required:[true,'Please Enter valid Address']},
+        kinEmailAddress : {type:String,required:[true,"Please Enter valid Email"]}
+    },
+    { collection : 'user_signup_data'},
+    { timestamps: true }
+)
+
+
+// Export Schema
+const investorKyc = mongoose.model('investorKyc',investorKyc);
+module.exports = investorKyc;
