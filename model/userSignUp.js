523955346d2b3e69b6b917fb7272158d9ea612ad
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-05 10:23:03 +0100

    installed validator

--- model/userSignUp.js
+++ model/userSignUp.js
@@ -1,12 +1,13 @@
 // Require Mongoose
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 // Create User Sign Up Schema
 const userSignUpSchema = new mongoose.Schema(
     {
         fullname : {type : String,required:true},
-        email : {type:String,required:true,unique:true,lowercase:true},
-        password: {type: String, required:true,minLength:8},
+        email : {type:String,required:[true,'Please enter an Email'],unique:true,lowercase:true,validate:[isEmail, 'Please Enter a Valid Email']},
+        password: {type: String, required:[true,'Please Enter a Password'],minlength:[8,'Minimum Password Length is 8 Characters']},
     },
     {collection:'user_signup_data'},
     {timestamps:true}
