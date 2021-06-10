7ce8e2e5d280840377f160ef6a15838e83d16a6c
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-10 15:40:19 +0100

    updated investment package

--- model/investmentPackage.js
+++ model/investmentPackage.js
@@ -13,17 +13,27 @@ const investmentPackagesSchema = new mongoose.Schema(
             type:Number,
             min:100,
             max:1000,
-            required:true,
+            required:true
+        },
+        VIPROIPercentage:{
+            type:Double,
+            default:0.15
         },
         ExclusiveInvestor:{
             type:String,
-            required:true,
+            required:true
         },
         ExclusiveCapital:{
             type:Number,
             min:1000,
-            required:true,
+            required:true
         },
+        EstimateExclusiveROIPercentage:{
+            type:Number,
+            min:1,
+            max:2,
+            required:True
+        }
     }
 )
 
