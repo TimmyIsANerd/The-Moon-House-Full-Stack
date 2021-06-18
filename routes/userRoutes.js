ab569898af2edd1d764a7707f1f5e4c60023115e
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-18 10:06:29 +0100

    set up new route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,14 @@ const userController = require('../controllers/userController');
 // Set Up Routes for User 
 // User Dashboard GET
 router.get('/dashboard',userController.user_dashboard_get);
+// User Dashboard Profile GET
+router.get('/dashboard/profile',userController.user_dashboard_profile_get);
+// User Dasboard Security GET
+router.get('/dashboard/security',userController.user_dashboard_security_get);
+// User Dashboard Notifications GET
+router.get('/dashboard/notifications',userController.user_dashboard_notifications_get);
+// User Dashboard History GET
+router.get('/dashboard/history',userController.user_dashboard_history_get);
 // User Dashboard Fund Account Post
 router.post('/dashboard/fund',userController.user_dashboard_fund_post);
 // User Create Investment Portfolio
