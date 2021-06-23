5f70a9590742c8872bfb206b45af4cc52874c8ef
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-23 21:31:06 +0100

    wrote config for nodemailer

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -1,11 +1,33 @@
+// Import Nodemailer Module
 const nodemailer = require('nodemailer');
+// Import auth config file (Contains Email Sender Credentials)
 const config = require('../config/auth.config');
-
 const {user,pass} = config;
 
+// Created a transport object and passed sender credentials
 const transport = nodemailer.createTransport({
     service: "Gmail",
     auth:{
-        user,pass
+        user,
+        pass
     }
 });
+
+// Export sendEmail Function
+module.exports.sendConfirmationEmail = (name,email,confirmationCode) =>{
+    console.log("Check");
+    transport.sendMail({
+        from : user,
+        to : email,
+        subject: "Please Confirm Your Account | The Moon House",
+        // Email Confirmation Content
+        html: `    
+        <div>
+            <h1>Email Confirmation</h1>
+            <h2>Hello ${firstName}</h2>
+            <p>Thank you for signing up to join the moon house <br> Please confirm your email by clicking on the following link</p>
+            <a href="http://localhost:4000/confirm/${confirmationCode}">Click Here</a>
+        </div>
+        `
+    }).catch(err => console.log(err));
+};
