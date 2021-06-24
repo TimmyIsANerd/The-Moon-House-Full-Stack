e498159099f4a773794e921c28ca7c19bc3c6d79
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-24 15:20:53 +0100

    added try/catch

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,40 @@
 // Import Sign Up Model
 const userSignUp = require('../model/userSignUp');
+const Role = require('../model/role');
+// Import nodemailer auth config
+const config = require('../config/auth.config');
+const nodemailer = require('../config/nodemailer.config');
 // Import Bcrypt
 const bcrypt = require('bcryptjs');
 // Import JWT
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'asdasnnnL@#*@#@$NFOQANOKNAODAOapsa';
-// Set Up Roles in User Account Creation
-const User = userSignUp.user;
-const
+
+// Creating JWT Secret
+const JWT_SECRET = function createJWTToken(){
+    const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let JWT_SECRET = '';
+    for (let i = 0; i < 25; i++) {
+        JWT_SECRET += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return JWT_SECRET;
+}
 // Set Up Sign Up Controller
 // GET Sign Up Page
 const sign_up_get = (req,res) =>{
     res.render('./user/userSignUp',{title:'User Sign Up'});
 }
+// Sign Up Success GET Request
+const sign_up_success_get = (req,res) =>{
+    res.render('user/signUpSuccess');
+}
 // POST Sign Up Page
 const sign_up_post = async (req,res)=>{
-    console.log(req.body);    
+    // Randomly Create Tokens for Confirmation for new Users
+    const characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let confirmationToken = '';
+    for (let i = 0;i < 25; i++){
+        confirmationToken += characters[Math.floor(Math.random() * characters.length )];
+    }
     const { firstName ,lastName ,email, password: plainTextPassword } = req.body;
     // Hashing Password using bcrypt library
     // Full Name Validation
@@ -29,31 +48,110 @@ const sign_up_post = async (req,res)=>{
     if(!plainTextPassword || typeof plainTextPassword !== 'string'){
         return res.json({status : error, error : 'Invalid Password'});
     }
-    if(plainTextPassword.length < 5){
-        return res.json({status : error, error : '{Password is less than 6 characters}'});
+    if(plainTextPassword.length < 8){
+        return res.json({status : error, error : 'Password is less than 8 characters'})
+        throw error;
     }
     const password = await bcrypt.hash(plainTextPassword, 10);
     // Use Try/Catch to Create User in the Database
+    // Create a New unverified user in the system
     try{
-        const response = await userSignUp.create({
+        const newUser = await userSignUp.create({
             firstName,
             lastName,
             email,
-            password
+            password,
+            status:'Active',
+            userType:'investor',
+            confirmationCode:confirmationToken
         })
-        console.log('User Created Successfully', response);
+        console.log('User Created Successfully', newUser)
+        // Use nested try/catch to create token
+        try{
+            // Create Token
+            const maxAge = 3 * 24 * 60 * 60;
+            const createToken = (id) =>{
+                return jwt.sign(
+                    { id },
+                    JWT_SECRET(),
+                    {expiresIn:maxAge}
+                );
+            }
+            // Create Cookie
+            const token = await createToken(newUser._id);
+            res.cookie('jwt', token, {httpOnly:true, maxAge : maxAge * 1000})
+            return res.json(newUser._id).status(201);
+            // Use nested try/catch to send confirmation email
+            try{
+                // Call the sendEmail Function in the signup method
+                newUser.save((err, user) =>{
+                    if(err) {
+                        res.status(500)
+                        return;
+                    }
+                    return res.json({
+                        status: "ok",
+                    });
+        
+        
+                    nodemailer.sendConfirmationEmail(
+                        newUser.firstName,
+                        newUser.email,
+                        newUser.confirmationCode
+                    )
+                })
+            } catch (error){
+                if(error){
+                    res.status(500).json(
+                        {
+                            status:'error',
+                            error:error
+                        }
+                    )
+                }
+            }
+
+        } catch (error){
+            if(error){
+                res.status(500).json(
+                    {
+                        status:'error',
+                        error:error
+                    }
+                )
+            }
+            throw error;
+        }
     } catch (error){
         if(error.code === 11000){
             // duplicate key error
-            return res.json({ status : 'error', error : 'Email already in use' });
+            return res.json({ status : 'error', error : 'Email is already registered' });
         }
         throw error;
     }
-    res.json({status : 'ok', redirect: '/user/signup_success' });
+    return res.json({status : 'ok'})
 }
-// Sign Up Success GET Request
-const sign_up_success_get = (req,res) =>{
-    res.render('user/signUpSuccess');
+// Verify User Account GET Request
+const verifyUser = (req,res,next) =>{
+    const User = userSignUp.findOne({
+        confirmationCode : req.params.confirmationCode,
+    })
+        .then((user) =>{
+            console.log(user);
+            if(!user){
+                return res.status(404).send({
+                    message:'User Not Found'
+                })
+            }
+            user.status = "Active";
+            user.save((err) =>{
+                if(err){
+                    res.status(500).send({message:err});
+                    return;
+                }
+            });
+        })
+        .catch((err) => console.log("error", err));
 }
 // Change Password Controller GET Request
 const change_password_get = (req,res) =>{
@@ -73,7 +171,7 @@ const login_get = (req,res) =>{
 }
 const login_post = async (req,res) =>{
 
-    // Store the body in deconstruction
+    // Store the incoming body in deconstructed object
     const { email, password } = req.body
     const user = await userSignUp.findOne({ email })
     console.log(req.body)
@@ -85,10 +183,16 @@ const login_post = async (req,res) =>{
             }
         )
     }
-    if(user.status != 'Active'){
-        return res.status(401).send({
-            message:"Pending Account. Please verify your Email!"
-        })
+    // Login Condition
+    // Incase of unverified account
+    if(await user.status != 'Active'){
+        return res.status(401).json(
+            {   
+                status:'error',
+                error:"Pending Account. Please verify your Email!",
+                redirect:'/error/unverifiedaccount'
+            }
+        )
     }
     if(await bcrypt.compare(password, user.password)){
         // The username, password combination is successful.
@@ -101,10 +205,10 @@ const login_post = async (req,res) =>{
                 {
                     status:'ok',
                     data:token,
-                    // redirect:'/dashboard'
+                    // redirect:`/dashboard/${user._id}`
                 }
             )
-            console.log('Created Token')
+            console.log('Got the Token')
         } catch(error){
             if(error){
                 return res.json({ status: 'error', error: 'Invalid Username/Password'})
@@ -122,6 +226,11 @@ const logout_get = (req,res) =>{
 
 }
 
+// Unverifed Account Attempting Login
+const unverifieduseraccount = (req,res) =>{
+    res.render('unverifiedlogin', {title:'Unverified Account'});
+}
+
 
 module.exports = {
     sign_up_get,
@@ -131,5 +240,7 @@ module.exports = {
     login_post,
     logout_get,
     change_password_get,
-    change_password_post
+    change_password_post,
+    verifyUser,
+    unverifieduseraccount
 }
