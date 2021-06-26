b6911b4f7c7bfe1e811104af48b3624cc536d79b
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-06-26 10:39:53 +0100

    wrote new middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -28,18 +28,18 @@ const requireAuth = (req,res,next) =>{
 
 // Check Current User
 const checkUser = (req,res,next) =>{
-    const token = req.cookie.jwt;
+    const token = req.cookies.jwt
 
     if(token){
         jwt.verify(token,JWT_SECRET, async (err,decodedToken)=>{
             if(err){
                 console.log(err.message)
-                res.locals.user = null;
-                next();
+                res.locals.user = null
+                next(); 
             } else {
                 console.log(decodedToken);
-                let user = await User.findbyId(decodedToken.id);
-                res.locals.user = user;
+                let user = await User.findById(decodedToken.id)
+                res.locals.user = user
                 next();
             }
         })
@@ -49,4 +49,26 @@ const checkUser = (req,res,next) =>{
     }
 }
 
-module.exports = { requireAuth, checkUser };
+// Dashboard Redirect
+const dashboardRedirect = (req,res,next) =>{
+    const token = req.cookies.jwt
+
+    // Check if token exists & is verified
+    if(token){
+        jwt.verify(token,JWT_SECRET,(err,decodedToken)=>{
+            if(err){
+                console.log(err.message)
+                next();
+            } else {
+                res.redirect('/dashboard');
+                console.log(decodedToken);
+                next();
+            }
+        })
+    } else {
+        // If cookie does not exist, redirect user to login
+        // res.redirect('/dashboard');
+        next();
+    }
+}
+module.exports = { requireAuth, checkUser, dashboardRedirect };
