f7fe41a9cf659c8a28b71cbf6c9363f310b850b3
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-08-04 20:17:05 +0100

    corrrected admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,26 +6,26 @@ const adminController = require('../controllers/adminController');
 
 // Setup Routes for Admin
 // Admin Sign Up GET
-router.get('/signup',adminController.admin_signup_get);
+router.get('/admin/signup',adminController.admin_signup_get);
 // Admin Sign Up POST
-router.get('/signup',adminController.admin_signup_post);
+router.get('/admin/signup',adminController.admin_signup_post);
 // Admin GET Login
-router.get('/login',adminController.admin_get);
+router.get('/admin/login',adminController.admin_get);
 // Admin POST Login
 router.post('/',adminController.admin_post);
 // Admin Dashboard
-router.get('/dashboard',adminController.admin_dashboard_get)
+router.get('/admin/dashboard',adminController.admin_dashboard_get)
 // Admin POST Create New User
-router.post('/dashboard/createuser',adminController.admin_dashboard_create_user);
+router.post('/admin/dashboard/createuser',adminController.admin_dashboard_create_user);
 // Admin DELETE User 
-router.delete('/dashboard/deleteuser/:id',adminController.admin_dashboard_delete_user);
+router.delete('/admin/dashboard/deleteuser/:id',adminController.admin_dashboard_delete_user);
 // Admin POST Set Kyc Information
-router.post('/dashboard/edituserkycData/:id',adminController.admin_dashboard_editUserInformation);
+router.post('/admin/dashboard/edituserkycData/:id',adminController.admin_dashboard_editUserInformation);
 // Admin Create User Portfolio
-router.post('/createuserportfiolio/:id',adminController.admin_dashboard_createUserPortfolio);
+router.post('/admin/createuserportfiolio/:id',adminController.admin_dashboard_createUserPortfolio);
 
 // User Notification
-router.get('/sendnotification',adminController.send_notification_get);
-router.post('/sendnotification',adminController.send_notification_post);
+router.get('/admin/sendnotification',adminController.send_notification_get);
+router.post('/admin/sendnotification',adminController.send_notification_post);
 
 module.exports = router;
