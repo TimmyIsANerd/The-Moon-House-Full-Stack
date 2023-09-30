5ab1ff037efd6c6e828c183a76109211561231a2
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-30 14:34:47 +0100

    update: prettified

--- controllers/basicController.js
+++ controllers/basicController.js
@@ -1,32 +1,34 @@
 // Home Page
-const home = (req,res) =>{
-    res.render('index',{title : "The Perfect Hub To Crypto Education & Growth."});
-}
+const home = (req, res) => {
+  res.render("index", {
+    title: "The Perfect Hub To Crypto Education & Growth.",
+  });
+};
 
 // Invest Page
-const invest = (req,res) =>{
-    res.render('invest',{title : "Let Us Build Your Wealth"});
-}
+const invest = (req, res) => {
+  res.render("invest", { title: "Let Us Build Your Wealth" });
+};
 
 // Contact Us Route
-const contact_us = (req,res) =>{
-    res.render('contact',{title : 'Contact Us'});
-}
+const contact_us = (req, res) => {
+  res.render("contact", { title: "Contact Us" });
+};
 
 // Privacy Policy
-const privacy_policy = (req,res) =>{
-    res.render('privacy',{title: 'Our Privacy Policy'});
-}
+const privacy_policy = (req, res) => {
+  res.render("privacy", { title: "Our Privacy Policy" });
+};
 
 // Terms & Conditions
-const terms = (req,res) =>{
-    res.render('terms', {title : 'Terms & Conditions'});
-}
+const terms = (req, res) => {
+  res.render("terms", { title: "Terms & Conditions" });
+};
 
 module.exports = {
-    home,
-    invest,
-    contact_us,
-    privacy_policy,
-    terms,
+  home,
+  invest,
+  contact_us,
+  privacy_policy,
+  terms,
 };
