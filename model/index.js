1fa5ba647c91c5912a8adb21f4937984257bca88
IamSolomonChika <solomon.chika1@gmail.com>
2023-10-09 00:06:38 +0100

    Admin and AdminDepositForUser Models added

---
+++ model/index.js
@@ -0,0 +1,17 @@
+// ==========================ADMIN MODELS============================
+import Admin from "./AdminModels/Admin.js";
+import AdminDepositForUser from "./AdminModels/AdminDepositForUser.js";
+
+
+// ==========================USER MODELS=============================
+import User from "./UserModels/User.js";
+import UserDeposit from "./UserModels/UserDeposit.js";
+import UserWithdrawal from "./UserModels/UserWithdrawal.js";
+import UserInvestmentAccount from "./UserModels/UserInvestmentAccount.js";
+import UserRunningInvestment from "./UserModels/UserRunningInvestment.js";
+import UserTransfer from "./UserModels/UserTransfer.js";
+import UserNextOfKin from "./UserModels/UserNextOfKin.js";
+
+
+export { Admin, AdminDepositForUser,
+    User, UserDeposit, UserWithdrawal, UserInvestmentAccount, UserRunningInvestment, UserTransfer, UserNextOfKin}
